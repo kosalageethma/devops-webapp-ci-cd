7985b7ee556ae4d52d06e268dbe5f2b1281ef33e
kosalageethma <kosalageethma95@gmail.com>
2025-06-11 11:53:44 +0530

    Fix: flake8 lint errors

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "🚀 Hello from your CI/CD Flask Web App!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
